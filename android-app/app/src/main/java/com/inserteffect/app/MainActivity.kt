bcae18a17e79f7b3bcc4302be5eb2c8579a468c3
Florian Anselstetter <florian.anselstetter@inserteffect.com>
2021-02-03 16:51:05 +0100

    Add new submodule

--- android-app/app/src/main/java/com/inserteffect/app/MainActivity.kt
+++ android-app/app/src/main/java/com/inserteffect/app/MainActivity.kt
@@ -2,11 +2,14 @@ package com.inserteffect.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.inserteffect.mylibrary.Library
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Library().init()
     }
 }
---
+++ android-app/mylibrary/src/main/java/com/inserteffect/mylibrary/Library.kt
@@ -0,0 +1,7 @@
+package com.inserteffect.mylibrary
+
+class Library {
+    fun init() {
+        // Do nothing
+    }
+}
